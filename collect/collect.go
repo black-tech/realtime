a2af62b0a8a9ca56a61cfdd78302645498a4d731
ckeyer <me@ckeyer.com>
2015-12-19 01:27:43 +0800

    add data

--- collect/collect.go
+++ collect/collect.go
@@ -2,6 +2,8 @@ package collect
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 	// "github.com/black-tech/realtime/lib/db"
 	// "gopkg.in/mgo.v2"
@@ -10,10 +12,44 @@ import (
 )
 
 var (
-	START_TIME  = time.Date(2012, 1, 1, 0, 0, 0, 0, time.Local)
+	START_TIME  = time.Date(2015, 3, 9, 0, 0, 0, 0, time.Local)
 	SOURCE_FILE = "/data/realtime.csv"
 	LOG_FILE    = "/data/realtime.log"
 )
+
+type Ball struct {
+	ID   int
+	Time time.Time
+	Cell []int
+}
+
+func NewBallByBytes(bs []byte) (*Ball, error) {
+	ss := strings.Split(string(bs), ",")
+	if len(ss) == 7 {
+		id, err := strconv.Atoi(ss[0])
+		if err != nil {
+			return nil, err
+		}
+
+		t, err := time.Parse("2006-01-02 15:04:05", ss[1])
+		if err != nil {
+			return nil, err
+		}
+		cell := make([]int, 5)
+		for i, v := range ss[2:] {
+			cell[i], err = strconv.Atoi(v)
+			if err != nil {
+				return nil, err
+			}
+		}
+		return &Ball{
+			ID:   id,
+			Time: t,
+			Cell: cell,
+		}, nil
+	}
+	return nil, fmt.Errorf("Error Ball Bytes ")
+}
 
 // checkExistingData  检查已有数据完整性
 func checkExistingData(path string, force bool) error {
@@ -32,7 +68,7 @@ func checkExistingData(path string, force bool) error {
 }
 
 func downloadAll() {
-	f, err := os.OpenFile(SOURCE_FILE, os.O_CREATE|os.O_RDWR, 0644)
+	f, err := os.OpenFile(SOURCE_FILE, os.O_APPEND|os.O_RDWR, 0644)
 	if err != nil {
 		panic(err)
 	}
@@ -67,4 +103,21 @@ func downloadAll() {
 			}
 		}
 	}
+}
+
+func GetLastLine() (*Ball, error) {
+	f, err := os.Open(SOURCE_FILE)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	buf := make([]byte, 41)
+	stat, err := os.Stat(SOURCE_FILE)
+	start := stat.Size() - 42
+	_, err = f.ReadAt(buf, start)
+	if err != nil {
+		return nil, err
+	}
+	return NewBallByBytes(buf)
 }
--- collect/collect_test.go
+++ collect/collect_test.go
@@ -1,10 +1,20 @@
 package collect
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestDownloadAll(t *testing.T) {
-	// return
+	return
 	downloadAll()
+}
+
+func TestGetLatestLine(t *testing.T) {
+	ball, err := GetLastLine()
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(ball)
+	fmt.Println(ball.ID % 1000)
 }
